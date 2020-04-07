0081c8a5bb14f9c678aacc5b354f82375db1b7b4
Yan <yanteppe@gmail.com>
2020-04-08 01:32:40 +0500

    Решена проблема с интеграцией Tomcat + JSF + Omnifaces.

--- src/main/java/com/library/Application.java
+++ src/main/java/com/library/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // scanBasePackages = {"com.library"} - указание в с какого пакета искать бины.
-@SpringBootApplication(scanBasePackages = {"com.library"})
-public class LibraryApplication {
+@SpringBootApplication
+public class Application {
 
    public static void main(String[] args) {
-      SpringApplication.run(LibraryApplication.class, args);
+      SpringApplication.run(Application.class, args);
    }
 }
--- src/main/java/com/library/ServletInitializer.java
+++ src/main/java/com/library/ServletInitializer.java
@@ -11,6 +11,6 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
    @Override
    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-      return application.sources(LibraryApplication.class);
+      return application.sources(Application.class);
    }
 }
